67d9e964abad94cf995638735deb2ebea5ba6fb9
Jefry Prasetyo <109359059+AerynSalon@users.noreply.github.com>
2025-08-21 00:45:06 +0700

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-// Konfigurasi aplikasi (v1.3.1)
-window.APP_CONFIG = {
-  SHEET_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbwlv_f8oz32Q07Vmk1w5ixWFlBLApT6n6k2_WQ8uPcxnAfdRZSQXpgl4uyeC5SMEgh_2Q/exec",
-  SALON_NAME: "AerynSalon",
-  SECRET: "1rXoIaFuu8rUtLrk4B5WphNiayUEx8BpPPn_f19DZ4U8",
-  TICKER_TEXT: "jangan lupa berdoa sebelum memulai aktifitas"
-};
